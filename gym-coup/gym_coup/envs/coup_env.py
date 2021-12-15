351fea0cbce68a64748cb077f0ba2335ec27f6dc
Brandon Starcheus <bstarcheus@gmail.com>
2021-12-15 14:50:06 -0500

    Add get_valid_actions. Global keywords for cards, actions. Empty funcs for actions.

--- gym-coup/gym_coup/envs/coup_env.py
+++ gym-coup/gym_coup/envs/coup_env.py
@@ -2,12 +2,37 @@ import gym
 import numpy as np
 from random import shuffle
 
-
+# Cards
 CARD_NAMES = ['Assassin',
               'Ambassador',
               'Captian',
               'Contessa',
               'Duke']
+ASSASSIN   = 0
+AMBASSADOR = 1
+CAPTAIN    = 2
+CONTESSA   = 3
+DUKE       = 4
+
+# Actions
+INCOME             = 0
+FOREIGN_AID        = 1
+COUP               = 2
+TAX                = 3
+ASSASSINATE        = 4
+EXCHANGE           = 5
+EXCHANGE_RETURN_12 = 6
+EXCHANGE_RETURN_13 = 7
+EXCHANGE_RETURN_14 = 8
+EXCHANGE_RETURN_23 = 9
+EXCHANGE_RETURN_24 = 10
+EXCHANGE_RETURN_34 = 11
+STEAL              = 12
+BLOCK              = 13
+CHALLENGE          = 14
+LOSE_CARD_1        = 15
+LOSE_CARD_2        = 16
+PASS_              = 17
 
 class Player:
     def __init__(self, id, is_human=False):
@@ -41,7 +66,11 @@ class Game:
         self.deal_cards()
 
         # P1 = 0, P2 = 1
+        # Whose overall turn is it?
         self.whose_turn = 0
+        # Turns can include several sub-actions.
+        # Who is currently choosing an action?
+        self.whose_action = 0
 
     def draw_card(self, index=0):
         return self.deck.pop(index)
@@ -53,6 +82,134 @@ class Game:
         for _ in range(2):
             for p in self.players:
                 p.add_card(self.draw_card())
+
+    def next_player_turn(self):
+        '''
+        Increment whose turn it is
+        Turns can include several sub-actions
+            ex: P1 Steal, P2 Block, P1 Challenge
+                is a single turn of 3 actions
+        '''
+        self.whose_turn = 1 - self.whose_turn
+
+    def next_player_action(self):
+        '''
+        Increment whose action it is
+        '''
+        self.whose_action = 1 - self.whose_action
+
+    def get_curr_action_player(self):
+        return self.players[self.whose_action]
+
+    def get_valid_actions(self):
+        curr_player = self.get_curr_action_player()
+        opp_player = self.players[1 - self.whose_action]
+
+        if self.whose_turn != self.whose_action:
+            # It is opp_player's turn, and curr_player can
+            # choose to block or challenge for certain actions
+
+            if opp_player.last_action == FOREIGN_AID:
+                return [PASS_, BLOCK]
+            elif opp_player.last_action in [TAX, EXCHANGE]:
+                return [PASS_, CHALLENGE]
+            elif opp_player.last_action == STEAL:
+                return [PASS_, BLOCK, CHALLENGE]
+            elif opp_player.last_action in [ASSASSINATE, COUP, CHALLENGE]:
+                # CHALLENGE will only reach here if the opp_player challenges a block,
+                # and the challenge succeeds (curr_player must lose a card)
+
+                ret = []
+                for i in range(2):
+                    if not curr_player.is_card_face_up[i]:
+                        # Card is still in play. Can choose to give it up.
+                        ret += [LOSE_CARD_1 + i]
+
+                if opp_player.last_action == ASSASSINATE:
+                    ret += [BLOCK, CHALLENGE]
+
+                return ret
+            else:
+                print("Error: invalid action progression")
+                return
+
+        elif opp_player.last_action == BLOCK:
+            # It is curr_player's turn and opp_player wants to block their move
+            return [PASS_, CHALLENGE]
+
+        else:
+            # It's the beginning of curr_player's turn
+
+            if curr_player.coins >= 10:
+                return [COUP]
+
+            ret = [INCOME, FOREIGN_AID, TAX, EXCHANGE]
+            if curr_player.coins >= 3:
+                ret.append(ASSASSINATE)
+            if curr_player.coins >= 7:
+                ret.append(COUP)
+            if opp_player.coins > 0:
+                ret.append(STEAL)
+
+            return ret
+
+
+    def income(self):
+        self.get_curr_action_player().add_coins(1)
+        self.next_player_action()
+        self.next_player_turn()
+
+    def foreign_aid(self):
+        ...
+
+    def coup(self):
+        ...
+
+    def tax(self):
+        ...
+
+    def assassinate(self):
+        ...
+
+    def exchange(self):
+        ...
+
+    def exchange_return_12(self):
+        ...
+
+    def exchange_return_13(self):
+        ...
+
+    def exchange_return_14(self):
+        ...
+
+    def exchange_return_23(self):
+        ...
+
+    def exchange_return_24(self):
+        ...
+
+    def exchange_return_34(self):
+        ...
+
+    def steal(self):
+        ...
+
+    def block(self):
+        ...
+
+    def challenge(self):
+        ...
+
+    def lose_card_1(self):
+        ...
+
+    def lose_card_2(self):
+        ...
+
+    def pass_(self):
+        ...
+
 
 
 class CoupEnv(gym.Env):
@@ -79,7 +236,7 @@ class CoupEnv(gym.Env):
         14: 'challenge',   # challenge the opponent's move
         15: 'lose_card_1', # choose which card to lose
         16: 'lose_card_2',
-        17: 'pass'         # only an option when asked whether you'd
+        17: 'pass_'         # only an option when asked whether you'd
                            # like to block or challenge your opponent's last move
     }
 
@@ -98,7 +255,7 @@ class CoupEnv(gym.Env):
         #     P2 # coins         (0 - 12)
         #     P1 last action
         #     P2 last action
-        #     Whose turn         (0, 1)
+        #     Whose next action  (0, 1)
         # Note: some observations will never occur in game
         #       ex: All 4 cards are the same. Both players have all cards face up.
         low  = np.zeros(9, dtype='uint8')
